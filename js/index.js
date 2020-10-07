f65123c0ad03a4c18400e86d53256227ca4da235
Александр <aleksandr.vesterovskiy@neuro-city.ru>
2020-10-07 12:45:05 +0300

    save

--- js/index.js
+++ js/index.js
@@ -1,49 +1,74 @@
 /* eslint-disable semi */
 /* eslint-disable no-multiple-empty-lines */
-
 class Panorama {
     constructor() {
         this.page = document.querySelector('.page');
+        this.bg = document.querySelector('.panorama')
 
-        this.bg = null
         this.bgStartPosX = 50
-        this.bgStartPosY = 50
         this.bgCurrentBgPosX = 0
+
+        this.bgStartPosY = 50
         this.bgCurrentBgPosY = 0
 
         this.startTouches = null
-        this.startTouchesX = 0
-        this.startTouchesY = 0
-
+        this.speed = 20
+        this.scale = 1
     }
 
     init() {
-        this.page.scrollLeft = 2100
-
-        console.log('INIT');
-        this.bg = document.querySelector('.panorama')
         this.bg.style.backgroundPosition = `${this.bgStartPosX}% ${this.bgStartPosY}%`
-        this.touchStart()
+        // this.touchStart()
+        // this.touchMove()
+        this.zoom()
     }
 
     touchStart() {
         this.bg.addEventListener('touchstart', event => {
+            event.preventDefault();
 
             this.startTouches = event.changedTouches[0]
             this.bgCurrentBgPosX = this.bgStartPosX
+            this.bgCurrentBgPosY = this.bgStartPosY
+        })
+    }
+    touchMove() {
+        this.bg.addEventListener('touchmove', event => {
+            event.preventDefault();
 
-            console.log(this.startTouchesX, this.startTouchesY);
+            const moveTouches = event.changedTouches[0];
+            const differenceStartMoveX = this.startTouches.pageX - moveTouches.pageX;
+            const differenceStartMoveY = this.startTouches.pageY - moveTouches.pageY;
+
+            this.bgStartPosX = this.bgCurrentBgPosX + (differenceStartMoveX / this.speed)
+            this.bgStartPosY = this.bgCurrentBgPosY + (differenceStartMoveY / this.speed)
+
+            if (this.bgStartPosX <= 0) this.bgStartPosX = 0
+            if (this.bgStartPosY <= 0) this.bgStartPosY = 0
+            if (this.bgStartPosX >= 100) this.bgStartPosX = 100
+            if (this.bgStartPosY >= 100) this.bgStartPosY = 100
+
+            this.bg.style.backgroundPosition = `${this.bgStartPosX}% ${this.bgStartPosY}%`
         })
     }
 
+    zoom() {
+        this.page.addEventListener('wheel', event => {
+            const stepScale = event.deltaY / 1000
+
+            this.scale = this.scale + 0.2 * -stepScale;
+            this.page.style.transform = `scale(${this.scale})`
+
+            if (this.scale <= 1) {
+                this.scale = 1
+                this.page.style.transform = `scale(${this.scale})`
+            }
+        })
+    }
 }
 
 const panorama = new Panorama()
 panorama.init()
-
-
-
-
 
 
 
